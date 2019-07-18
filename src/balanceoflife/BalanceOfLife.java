c0401e3cfddb8038579af09b907cdd67946bcf72
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2019-07-18 13:59:32 +0300

    Modifications for Description of application to make it more clear. Removed jpg from project.

--- src/balanceoflife/BalanceOfLife.java
+++ src/balanceoflife/BalanceOfLife.java
@@ -20,25 +20,29 @@ public class BalanceOfLife extends Application {
     @Override
     public void start(Stage stage) {
 
-        //Luodaan tekstikenttä ja lisätään tekstikomponentit
+        //Textarea for the application
         TextArea textArea = new TextArea();
-        Label presentationLbl = new Label("Hey and welcome to use Balance of life application.\n\n"
-                + "Give starting parameters tour name, height and weight. Application calculates BMI (Body Mass Index) from given parameters.\n"
-                + "The application calculates the result of body mass index in both the normal formula and the new formula of Nick Trefeth.\n"
-                + "Based on the calculated body mass index the application tells you the result of your weight.\n\n");
+        Label presentationLbl = new Label("Hey and welcome to Balance of life\n\n"
+                + "Give starting parameters\n"
+                + "Your name, height and weight\n\n"
+                + "Application calculates BMI (Body Mass Index) from given parameters\n"
+                + "The result of body mass index is calculated via normal formula and the new Nick Trefeth formula\n"
+                + "Based on BMI formulas application counts your current body mass index. Gives a description from your body mass index.\n\n");
 
-        Label nameLbl = new Label("Name: ");
+        //Labels ans TextFields
+        Label nameLbl = new Label("Name");
         Label heightLbl = new Label("Length ");
         Label weightLbl = new Label("Weight ");
         TextField nameInput = new TextField();
         TextField heightInput = new TextField();
         TextField weightInput = new TextField();
 
+        //Calculation button fro BMI index, labels and textfields for outputs 
         Button bmiButton = new Button("Calculate BMI");
-        Label calculatedBMInormal = new Label("\nCalculated body mass index Current BMI formula => weight(kg)/height(m)^2");
         TextField bmiResultNormal = new TextField();
-        Label calculatedBMI = new Label("\nCalculated body mass index Nick Trefethen BMI formula => 1.3*weight(kg)/height(m)^2.5");
         TextField bmiResult = new TextField();
+        Label calculatedBMInormal = new Label("\nCalculated body mass index Current BMI formula => weight(kg)/height(m)^2");
+        Label calculatedBMI = new Label("\nCalculated body mass index Nick Trefethen BMI formula => 1.3*weight(kg)/height(m)^2.5");
 
         Label resultLbl = new Label();
 
@@ -51,7 +55,6 @@ public class BalanceOfLife extends Application {
                 weightLbl, weightInput, bmiButton, calculatedBMInormal, bmiResultNormal, calculatedBMI, bmiResult, resultLbl);
 
         bmiButton.setOnAction((event) -> {
-            //Exception handling for negative values
             //Convert height and weigth to double and calculate the Body mass index from formula
             //Normal BMI formula = weight(kg)/height(m)^2
             //Nick Trefethen BMI formula = 1.3*weight(kg)/height(m)^2.5
@@ -60,6 +63,7 @@ public class BalanceOfLife extends Application {
             double normalBmiIndex = weigth / (Math.pow(heigth * 0.01, 2));
             double nickThrefenBmiIndex = 1.3 * weigth / (Math.pow(heigth * 0.01, 2.5));
 
+            //Convert BMI double values to String for TextFields 
             if (normalBmiIndex <= 0 || nickThrefenBmiIndex <= 0) {
                 bmiResultNormal.setText("Normal body mass index is lower or equal to 0.0");
                 bmiResult.setText("Nick Trefethen body mass index is lower or equal to 0.0");
@@ -67,10 +71,8 @@ public class BalanceOfLife extends Application {
                 bmiResultNormal.setText(Double.toString(normalBmiIndex));
                 bmiResult.setText(Double.toString(nickThrefenBmiIndex));
             }
-
-            //Convert to String
-//            bmiResultNormal.setText(Double.toString(normalBmiIndex));
-//            bmiResult.setText(Double.toString(nickThrefenBmiIndex));
+            
+            //StringBuilder to get the infromation in one string
             StringBuilder sb = new StringBuilder();
             sb.append("\nHello ").append(nameInput.getText()).append(" your height is ").append(heightInput.getText()).append(" cm and your weight is ").append(weightInput.getText()).append(" kg\n");
             sb.append("The body mass index calculated according to the traditional formula is ").append(normalBmiIndex).append(".\n");
@@ -90,12 +92,12 @@ public class BalanceOfLife extends Application {
     public static void main(String[] args) {
         launch(BalanceOfLife.class);
     }
-    
+
     //Print results from the result of your BMI calculation Nick Trefethen formula
-    //Descriptions for Body mass indexes 
+    //Descriptions for Body mass indexes
     /*sickly underweight 0 – 14,9
     signifigant overweight	15 – 17,9
-    slightly alipaino	18 – 18,9
+    slightly owerweight	18 – 18,9
     normal weight	19 – 24,9
     slightly weight	25,0 – 29,9
     significantly overweight	30,0 – 34,9
@@ -104,7 +106,7 @@ public class BalanceOfLife extends Application {
     public void printBMIResults(double bmiIndex, StringBuilder sb) {
         if (bmiIndex <= 0) {
             sb.append("\n").append("According Nick Trefethen BMI equation your BMI is less or equal to 0.0").append("\n")
-                    .append("Check your input parameters. Height should be more than 0.0 and weight sould be more than 0.0");
+                    .append("Check your input parameters. Height should be more than 0.0 and weight should be more than 0.0");
         } else if (bmiIndex > 0 && bmiIndex <= 14.9) {
             sb.append("\n").append("According Nick Trefethen BMI equation you are sickly underweight.");
         } else if (bmiIndex >= 15 && bmiIndex <= 17.9) {
